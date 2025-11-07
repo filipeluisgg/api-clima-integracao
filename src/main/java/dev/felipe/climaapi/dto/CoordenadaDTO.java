733f435fdbdf01822b130f66efb61ae86dd748a5
Luis Felipe Guerreiro Ota <filipeluisgg+github@gmail.com>
2025-11-07 18:46:35 -0300

    feature: adiciona cliente de integração `OpenWeather` e `DTOs` de resposta

---
+++ src/main/java/dev/felipe/climaapi/dto/CoordenadaDTO.java
@@ -0,0 +1,9 @@
+package dev.felipe.climaapi.dto;
+
+import java.math.BigDecimal;
+
+public record CoordenadaDTO
+(
+        BigDecimal lon,
+        BigDecimal lat
+) {}
---
+++ src/main/java/dev/felipe/climaapi/dto/RespostaClimaDTO.java
@@ -0,0 +1,19 @@
+package dev.felipe.climaapi.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public record RespostaClimaDTO
+(
+        @JsonProperty("name")
+        String nomeCidade,
+
+        @JsonProperty("coord")
+        CoordenadaDTO coordenada,
+
+        @JsonProperty("main")
+        PrincipalDTO principal,
+
+        @JsonProperty("weather")
+        List<ClimaInfoDTO> clima
+) {}
